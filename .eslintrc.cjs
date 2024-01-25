34a2072842d900a70e34acb0023564cafea00de2
masedri <marck.rodriguez09@gmail.com>
2024-01-25 15:29:04 +0100

    feat: add support to typescript and prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,67 @@
+module.exports = {
+  root: true,
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+  env: { node: true, 'vitest-globals/env': true },
+  extends: [
+    'plugin:vitest-globals/recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'standard',
+    '@vue/typescript/recommended',
+    'prettier',
+  ],
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project: './tsconfig.json',
+      },
+    },
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.vue'],
+    },
+  },
+  plugins: ['@typescript-eslint', 'prettier', 'import'],
+  ignorePatterns: ['node_modules/', 'dist/'],
+  rules: {
+    'import/no-unresolved': 0, // Managed by typescript
+    // 'no-console': [import.meta.env.NODE_ENV === 'production' ? 'error' : 'warn', { allow: ['error'] }],
+    // 'no-debugger': import.meta.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'vue/block-order': [
+      'error',
+      {
+        order: ['template', 'script', 'style'],
+      },
+    ],
+    'comma-dangle': ['error', 'always-multiline'],
+    'import/order': [
+      'error',
+      {
+        alphabetize: { order: 'asc', caseInsensitive: true },
+        'newlines-between': 'always',
+        groups: [
+          'builtin',
+          'external',
+          'index',
+          'parent',
+          'sibling',
+          'internal',
+        ],
+      },
+    ],
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: {
+          max: 2,
+        },
+        multiline: {
+          max: 1,
+        },
+      },
+    ],
+  },
+}

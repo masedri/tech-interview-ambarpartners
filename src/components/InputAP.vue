f56dcb005dbd7e328f6a1cfe2843556f940648e4
masedri <marck.rodriguez09@gmail.com>
2024-01-29 11:17:17 +0100

    feat: add InputAP component

---
+++ src/components/InputAP.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <label
+      class="font-semibold block my-3 text-md"
+      for="username"
+    >
+      {{ $attrs.label }}
+    </label>
+    <input
+      v-model="input"
+      v-bind="$attrs"
+      class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none"
+      @change="showError = !validation()"
+    />
+    <span
+      v-if="showError"
+      class="text-xs text-red-500"
+      >{{ errorMessage }}</span
+    >
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{
+  errorMessage: string
+  validation: () => boolean
+}>()
+defineOptions({
+  inheritAttrs: false,
+})
+const input = defineModel()
+const showError = ref(false)
+</script>

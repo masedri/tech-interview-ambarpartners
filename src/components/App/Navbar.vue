c30a23a54fa62a55d284e32d7c33a1d08148ba17
masedri <marck.rodriguez09@gmail.com>
2024-01-30 20:14:14 +0100

    feat: add redesign views and add icons

--- src/components/App/Navbar.vue
+++ src/components/App/Navbar.vue
@@ -1,11 +1,30 @@
 <template>
-  <nav class="bg-primary p-4 flex items-center justify-end">
-    <div class="text-secondary text-xl font-semibold">Frontend challenge</div>
+  <nav class="py-4 px-6 w-full" :class="[isWhite ? 'bg-white' : 'bg-black']">
+    <div class="w-60 cursor-pointer hover:scale-105 transition">
+      <AmabarParners
+        :class="[isWhite ? 'fill-black' : 'fill-white']"
+        @click="goToHome"
+      ></AmabarParners>
+    </div>
   </nav>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
+
+import AmabarParners from '@/assets/icons/AmbarPartners.svg'
+
 defineOptions({
   name: 'AppNavbar',
 })
+
+const router = useRouter()
+const route = useRoute()
+
+const isWhite = computed(() =>
+  ['intro', 'stepper-info'].includes(route.name as string)
+)
+
+const goToHome = () => router.push('/')
 </script>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,7 +3,6 @@
     <AppNavbar />
 
     <div class="overflow-y-auto flex flex-1">
-      <AppSidebar />
       <main class="flex-1 overflow-y-auto p-4">
         <slot />
       </main>
@@ -11,9 +10,8 @@
   </div>
 </template>
 
-<script setup lang="ts">
+<script setup>
 import AppNavbar from '@/components/App/Navbar.vue'
-import AppSidebar from '@/components/App/Sidebar.vue'
 
 defineOptions({
   name: 'MainLayout',
